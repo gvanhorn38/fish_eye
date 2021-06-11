96cb2afac10fa616451be93d14c5655b1d09e011
justinkay <justinkay92@gmail.com>
2021-06-11 19:47:21 +0000

    Fixed bug in Tracker.finalize which caused incorrect counts due to sorting. Simplified the logic a bit.

--- tracker.py
+++ tracker.py
@@ -37,59 +37,52 @@ class Tracker:
         self.frame_id += 1
     
     def finalize(self, output_path=None, min_length=-1.0): # vert_margin=0.0
-        fish_id_map = {}
-        fish_start_end = {}
         json_data = deepcopy(self.json_data)
-
+            
+        # map (valid) fish IDs to 0, 1, 2, ...
+        fish_id_map = {}
         for fish_id, count in self.fish_ids.items():
             if count >= self.min_hits:
                 fish_id_map[fish_id] = len(fish_id_map)
-                fish_start_end[fish_id] = [[], []]
 
+        # separate frame boxes into tracks, keyed by mapped IDs
+        # each track is a list of tuples ( bbox, frame_num )
+        tracks = { v : [] for _, v in fish_id_map.items() }
+        for frame in json_data['frames']:
+            for bbox in frame['fish']:
+                # check if valid
+                if bbox['fish_id'] in fish_id_map.keys():
+                    track_id = fish_id_map[bbox['fish_id']]
+                    tracks[track_id].append((bbox['bbox'], frame['frame_num']))
+
+        # map IDs and keep frame['fish'] sorted by ID
         for i, frame in enumerate(json_data['frames']):
             new_frame_entries = []
             for frame_entry in frame['fish']:
                 if frame_entry['fish_id'] in fish_id_map:
-                    if not fish_start_end[frame_entry['fish_id']][0]:
-                        fish_start_end[frame_entry['fish_id']][0] = [i, len(new_frame_entries)]
-                    fish_start_end[frame_entry['fish_id']][1] = [i, len(new_frame_entries)]
                     frame_entry['fish_id'] = fish_id_map[frame_entry['fish_id']]
                     new_frame_entries.append(frame_entry)
             frame['fish'] = sorted(new_frame_entries, key=lambda k: k['fish_id'])
 
+        # create summary 'fish' entry for json data
         json_data['fish'] = []
-        for track_id, (start_frame_index, end_frame_index) in enumerate(fish_start_end.values()):
+        for track_id, boxes in tracks.items():
             fish_entry = {}
             fish_entry['id'] = track_id
-
-            # top = False
-            # bottom = False
-            # for frame in json_data['frames']:
-            #     for frame_entry in frame['fish']:
-            #         if frame_entry['fish_id'] == track_id:
-            #             if frame_entry['bbox'][3] > vert_margin:
-            #                 top = True
-            #             if frame_entry['bbox'][1] < 1 - vert_margin:
-            #                 bottom = True
-            #             break
-
-            # if not top or not bottom:
-            #     continue
-
             fish_entry['length'] = -1
-
-            start_bbox = json_data['frames'][start_frame_index[0]]['fish'][start_frame_index[1]]['bbox']
-            end_bbox = json_data['frames'][end_frame_index[0]]['fish'][end_frame_index[1]]['bbox']
-
+            
+            start_bbox = boxes[0][0]
+            end_bbox = boxes[-1][0]
             fish_entry['direction'] = Tracker.get_direction(start_bbox, end_bbox)
-            fish_entry['start_frame_index'] = start_frame_index[0]
-            fish_entry['end_frame_index'] = end_frame_index[0]
+            
+            fish_entry['start_frame_index'] = boxes[0][1]
+            fish_entry['end_frame_index'] = boxes[-1][1]
             fish_entry['color'] = Tracker.selectColor(track_id)
 
             json_data['fish'].append(fish_entry)
 
+        # filter tracks by length
         json_data = Fish_Length.add_lengths(json_data)
-
         if min_length != -1.0:
             new_fish = []
             for fish in json_data['fish']:
